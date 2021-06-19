199df12bad79b0c4e1c6bae40fae404d1da275d3
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-06-19 09:41:04 +0300

    render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -12,13 +12,16 @@ export async function getStaticProps({ params }) {
 
 export default function Post({ postData }) {
 	return(
-		<Layout>
-			{postData.title}
-			<br />
-			{postData.id}
-			<br />
-			{postData.date}
-		</Layout>
+
+	  <Layout>
+		{postData.title}
+		<br />
+		{postData.id}
+		<br />
+		{postData.date}
+		<br />
+		<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          </Layout>
 	)
 }
 
